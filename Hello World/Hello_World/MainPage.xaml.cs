2bf0dafd62d9f2a01f974a7632a1c6dd7cce7ddc
cooperluetje <cooper@bytelion.com>
2017-11-07 12:31:56 -0500

    Add project files.

---
+++ Hello World/Hello_World/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Test_Library;
+
+namespace Hello_World
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            helloLabel.IsVisible = !helloLabel.IsVisible;
+            Class1 testClass = new Class1();
+            testClass.printHelloWorld();
+        }
+    }
+}
